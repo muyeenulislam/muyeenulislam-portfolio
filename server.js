bb34a577357d4670debd59a405733d29e8e12f12
muyeen <muyeenulislamsakif@gmail.com>
2022-11-19 13:09:56 +0600

    server

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE;
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
 
 mongoose.connect(DB).then(() => console.log('successful'));
 
